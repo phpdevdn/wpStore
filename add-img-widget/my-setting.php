8710f9e778c30e32bd28757d8876da72d147e59d
phpdevdn <khacduy0103@gmail.com>
2015-10-03 16:20:47 +0700

    update my-setting

--- add-img-widget/my-setting.php
+++ add-img-widget/my-setting.php
@@ -1,11 +1,11 @@
 <?php 
    /* enqueue css and js to admin*/
-
-   if(is_admin()){
-   	wp_enqueue_media();
+   function my_admin_scripts(){
+    wp_enqueue_media();
    	wp_enqueue_style('my_admin_style', get_template_directory_uri().'/inc/css/my-admin.css', false, '1.0' );
    	wp_enqueue_script( 'my_admin_script', get_template_directory_uri().'/inc/js/my-admin-script.js', false, '1.0' );
-   }
+    }
+   add_action('admin_enqueue_scripts',my_admin_scripts);
 
    /*end enqueue*/
  ?>
